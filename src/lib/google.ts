426075b22b8d953375c56c9d7f531eefb6bd5f47
Your Name <manticarodrigo@gmail.com>
2023-09-06 13:36:47 -0400

    Fix newline char parsing

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -8,7 +8,10 @@ const processorId = "99eeb3da01c05ed9"
 const client = new DocumentProcessorServiceClient({
   apiEndpoint: "us-documentai.googleapis.com",
   credentials: {
-    private_key: process.env.GOOGLE_DOCUMENTAI_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_DOCUMENTAI_PRIVATE_KEY?.replace(
+      /\\n/g,
+      "\n"
+    ),
     client_email: "commodity-ai@commodity-ai.iam.gserviceaccount.com",
   },
 })
